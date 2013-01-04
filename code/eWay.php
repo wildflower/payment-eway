8b110c3d70842d06e1a4e78fa6308330a503fe1a
haydn flower <wildflower1975@gmail.com>
2013-01-04 21:32:42 +1300

    it updates the payment an order
    after the transaction has been done on the eWay site it changes the
    order and payment status' to paid/success.
    put some _config variables in place

--- code/eWay.php
+++ code/eWay.php
@@ -8,15 +8,44 @@
 
 class eWay extends Payment{
 	
+	protected static $eway_CustomerID;
+	
+	public static function set_eway_CustomerID($account) {
+		self::$eway_CustomerID = $account;
+	}
+	public static function eway_CustomerID() {
+		return self::$eway_CustomerID;
+	}
+	protected static $eway_Username;
+	
+	public static function set_eway_Username($username) {
+		self::$eway_Username = $username;
+	}
+	public static function eway_Username() {
+		return self::$eway_Username;
+	}
 	static $db = array(
 		//it's a good idea to save transaction details that are returned from the gateway
-		//'TransactionID' => 'Varchar'
+		'TrxnNumber' => 'Varchar',
+		'AuthCode' => 'Varchar'
+	);
+	public static $summary_fields = array(
+	'TrxnNumber' => 'Txn Number'
 	);
 	
 	static $eWay_Url = "https://www.ewaygateway.com/gateway_CVN/payment.asp";
 	static $eWay_Sandbox_API_Key = "C3AB9CRwb7s70t3pKEaWxV8/CZnpt51SM0CmglO6Q0L3y6+r0yIidynMnHMpfzk1ug5YyH";
 
-	
+	public static function fetch_data($string, $start_tag, $end_tag)
+		{
+			$position = stripos($string, $start_tag);  
+			$str = substr($string, $position);  		
+			$str_second = substr($str, strlen($start_tag));  		
+			$second_positon = stripos($str_second, $end_tag);  		
+			$str_third = substr($str_second, 0, $second_positon);  		
+			$fetch_data = trim($str_third);		
+			return $fetch_data; 
+		}
 	//store logo url, api_endpoint, and config settings up here
 	
 	/**
@@ -33,8 +62,8 @@ class eWay extends Payment{
 	//	SS_Log::log( New Exception('nice currency Amount is :'.$this->Amount->Nice($options). ' amount amount is:'.$this->Amount->Amount), SS_Log::NOTICE );
 		
 		static $ewayurl;
-		$ewayurl.="?CustomerID=91369113";
-		$ewayurl.="&UserName=admin@scubapro.co.nzsand";
+		$ewayurl.="?CustomerID=".$this->eway_CustomerID();
+		$ewayurl.="&UserName=".$this->eway_Username();
 		$ewayurl.="&Amount=".$this->Amount->Nice($options);
 		$ewayurl.="&Currency=".Payment::site_currency();
 		$ewayurl.="&PageTitle=MyeWayTitle";
@@ -79,20 +108,8 @@ class eWay extends Payment{
 		$response = curl_exec($ch);
 		//SS_Log::log( New Exception($response), SS_Log::NOTICE );
 		
-		function fetch_data($string, $start_tag, $end_tag)
-		{
-			$position = stripos($string, $start_tag);  
-			$str = substr($string, $position);  		
-			$str_second = substr($str, strlen($start_tag));  		
-			$second_positon = stripos($str_second, $end_tag);  		
-			$str_third = substr($str_second, 0, $second_positon);  		
-			$fetch_data = trim($str_third);		
-			return $fetch_data; 
-		}
-		
-		
-		$responsemode = fetch_data($response, '<result>', '</result>');
-	    $responseurl = fetch_data($response, '<uri>', '</uri>');
+		$responsemode = eWay::fetch_data($response, '<result>', '</result>');
+	    $responseurl = eWay::fetch_data($response, '<uri>', '</uri>');
 		   
 		if($responsemode=="True")
 		{ 			  	  	
@@ -135,7 +152,7 @@ class eWay extends Payment{
 class eWay_Controller extends Controller{
 	
 	protected $payment = null; //only need to get this once
-	static $URLSegment = "ewayctl";
+	static $URLSegment = "ewayctl";	 
 	
 	static $allowed_actions = array(
 		'success',
@@ -145,15 +162,35 @@ class eWay_Controller extends Controller{
 	
 	
 	function payment(){
+		SS_Log::log( New Exception('In payment'), SS_Log::NOTICE );
+		$id = $this->request->param('ID');
+	
 		if($this->payment){
 			return $this->payment;
 		}
-		
-		if($id = Controller::getRequest()->getVar('paymentid')){
-			$p =  DataObject::get_one('MyPayment',"\"ID\" = '$id'");
+
+		if($id){
+			$p =  DataObject::get_one('payment',"\"OrderID\" = '$id'");
 			$this->payment = $p;
 			return $p;
 		}
+		return null;
+	}
+	
+	function order(){
+		SS_Log::log( New Exception('In Order'), SS_Log::NOTICE );
+		$id = $this->request->param('ID');
+	
+		if($this->order){
+		return $this->order;
+		}
+		
+		if($id){
+			$o =  DataObject::get_one('order',"\"ID\" = '$id'");
+			$this->order = $o;
+			return $o;
+		}
+		
 		return null;
 	}
 	
@@ -162,30 +199,82 @@ class eWay_Controller extends Controller{
 	}
 	function getAccessPaymentCode()
 	{
-		if($AccessPaymentCode = $this->request->postVar('AccessPaymentCode')){
-			SS_Log::log( New Exception('in getAccessPaymentCode is :'.$AccessPaymentCode), SS_Log::NOTICE );
-		}
-	
+		//return 'XAl9yWwl7aWYBY7rPCdAhzOQToMhCcLhodvkrCCmlGmSi7nQqC';
+		return $this->request->postVar('AccessPaymentCode');
 	}
 	
 	function success(){
-	SS_Log::log( New Exception('In Success AccessPaymentCode is :'.$AccessPaymentCode), SS_Log::NOTICE );
-		if($AccessPaymentCode = $this->request->postVar('AccessPaymentCode')){
-			SS_Log::log( New Exception('AccessPaymentCode is :'.$AccessPaymentCode), SS_Log::NOTICE );
+	
+	
+	if(eWay::eway_CustomerID()){
+		$CustomerID = eWay::eway_CustomerID();
 		}
-		//get payment object, based on some passed parameter AccessPaymentCode
-		$this->getAccessPaymentCode();
-		//curl to the result URL https://au.ewaygateway.com/Result/?CustomerID=87654321&UserName=TestAccount &AccessPaymentCode=611a5cabc19330f52f9db09e4549c225dda64a71aa8775f53 cafce75c0acff0b611a5cabc19330f52f9db09e4549c225dda64a71aa8775f5asdfalkji323jlJS
+	if(eWay::eway_Username()){
+		$Username = eWay::eway_Username();
+		}
+	
+	//curl to the result URL https://au.ewaygateway.com/Result/?CustomerID=87654321&UserName=TestAccount &AccessPaymentCode=611a5cabc19330f5
+	static $ewayurl;
+		$ewayurl.="?CustomerID=".$CustomerID;
+		$ewayurl.="&UserName=".$Username;
+		$ewayurl.="&AccessPaymentCode=".$this->getAccessPaymentCode();
+		
+		 $spacereplace = str_replace(" ", "%20", $ewayurl);	
+	    $posturl="https://au.ewaygateway.com/Result/$spacereplace";
+		SS_Log::log( New Exception('PostURL is :'.$posturl), SS_Log::NOTICE );
+		
+	
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $posturl);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		
+		$response = curl_exec($ch);
+		//SS_Log::log( New Exception($response), SS_Log::NOTICE );			
+		
+		//get payment/order object, based on some passed URL parameter
 		$payment = $this->payment();
+		$order = $this->order();
+		
+		//get status from response and stick it into $this->payment->status		
+		$responsestatus = eWay::fetch_data($response, '<ResponseCode>', '</ResponseCode>');
+		if($responsestatus =='00'){
+			$this->payment->Status = "Success";
+			$this->order->Status = "Paid";
+			$this->order->Paid = date('Y-m-d H:i:s');
+		}elseif ($responsestatus =='08'||$responsestatus =='10'||$responsestatus =='11'||$responsestatus =='16'){
+			$this->payment->Status ="Incomplete";
+		}else{
+			$this->payment->Status ="Failure";
+			}
+		$responsetxn = eWay::fetch_data($response, '<TrxnNumber>', '</TrxnNumber>');
+			$this->payment->TrxnNumber = $responsetxn;
+			
+		$responsemessage = eWay::fetch_data($response, '<TrxnResponseMessage>', '</TrxnResponseMessage>');
+		$this->payment->Message = $responsemessage;
+
+		$responseamount = eWay::fetch_data($response, '<ReturnAmount>', '</ReturnAmount>');
+		//get message from response and stick it into $this->payment->message
+		if ($responseamount != $this->payment->AmountAmount){
+			$this->payment->Status = "Failure";
+			$this->payment->Message = "Amount is different!";
+			$this->order->Status = "Unpaid";
+		}
+		
+		$this->payment->write();
+		$this->order->write();
+		//SS_Log::log( New Exception(var_export($payment,TRUE)), SS_Log::NOTICE );
 		
 		if($payment && $obj = $payment->PaidObject()){
 			//redirect to PaymentObject link (eg $order->Link())
-			Controller->redirect($obj->Link());
+			$link = $obj->Link();
+			$this->redirect($link);
 			return;
 		}
 		
 		//else redirect home
-		Controller->redirect(Director::absoluteURL('home',true));
+		$this->redirect(Director::absoluteURL('home',true));
 		return;
 	}
 	
